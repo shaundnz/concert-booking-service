c0b987c92d9a3ea234e76112ad779636ade8ee94
Shaun Price <shaundnz@gmail.com>
2021-04-27 22:13:47 +1200

    Subscribers persisted in memory. Requires fix to percentage calculations.

--- concert-common/src/main/java/asg/concert/common/dto/ConcertInfoSubscriptionDTO.java
+++ concert-common/src/main/java/asg/concert/common/dto/ConcertInfoSubscriptionDTO.java
@@ -1,5 +1,9 @@
 package asg.concert.common.dto;
 
+import asg.concert.common.jackson.LocalDateTimeDeserializer;
+import asg.concert.common.jackson.LocalDateTimeSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
@@ -36,6 +40,8 @@ public class ConcertInfoSubscriptionDTO {
         this.concertId = concertId;
     }
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     public LocalDateTime getDate() {
         return date;
     }
